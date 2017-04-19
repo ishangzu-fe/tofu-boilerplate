eef0e2737b1bde37cbd4ce83df78529bdcbc3a8c
wchen <406653843@qq.com>
2017-04-19 14:54:26 +0800

    update

--- src/mock/index.js
+++ src/mock/index.js
@@ -1,12 +1,16 @@
 import Number from './number';
+import config from 'config';
+
+// 请在根目录下配置是否使用 mock 环境
+if (!conig.mock) {
+    fetchMock.restore();
+}
 
 // 使用 fetch-mock 模块来进行请求的拦截
 // 文档请参考 http://www.wheresrhys.co.uk/fetch-mock/api
 const fetchMock = require('fetch-mock');
 
+// 匹配模式，拦截指定的请求
 fetchMock.mock('*', {
     a: Number.test
 });
-
-// 取消 mock
-// fetchMock.restore();
